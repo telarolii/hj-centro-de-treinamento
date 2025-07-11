b604efca1206a7aaada6113e7a478e38e0cce34a
telarolii <dev.telaroli@gmail.com>
2025-07-11 17:09:29 -0300

    navbar sem responsividade completa

--- frontend/components/Navbar/Navbar.jsx
+++ frontend/components/Navbar/Navbar.jsx
@@ -4,6 +4,9 @@ import { NavLink, Link } from "react-router-dom";
 // Hooks
 import { useState } from "react";
 
+// CSS Styles
+import "./Navbar.css"
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -11,18 +14,18 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <Link to="/">
+      <Link to="/" className="logo-link">
         <img
           src="/logo.png"
           alt="Logo do Centro de Treinamento HJ"
           className="logo"
         />
+      </Link>
 
-        <div className="hamburger" onClick={toggleMenu}>
+        <div className="hamburguer" onClick={toggleMenu}>
           &#9776;
         </div>
 
-      </Link>
       <ul className="nav-links">
         <li>
           <NavLink to="/">Home</NavLink>
