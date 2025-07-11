38d3204846302da70e4dee4f22aaf2c427bcd5de
telarolii <dev.telaroli@gmail.com>
2025-07-11 16:01:48 -0300

    Logo path fixed and responsive hamburger icon added.

--- frontend/components/Navbar/Navbar.jsx
+++ frontend/components/Navbar/Navbar.jsx
@@ -1,24 +1,45 @@
-import { NavLink } from "react-router-dom";
+// React Router Dom
+import { NavLink, Link } from "react-router-dom";
+
+// Hooks
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <nav className="navbar">
-      <img src="/logo.png" alt="Logo do Centro de Treinamento HJ" />
-      <ul className="navlinks">
+      <Link to="/">
+        <img
+          src="/logo.png"
+          alt="Logo do Centro de Treinamento HJ"
+          className="logo"
+        />
+
+        <div className="hamburger" onClick={toggleMenu}>
+          &#9776;
+        </div>
+
+      </Link>
+      <ul className="nav-links">
         <li>
-            <NavLink to="/">Home</NavLink>
+          <NavLink to="/">Home</NavLink>
         </li>
         <li>
-            <NavLink to="/modalidades">Modalidades</NavLink>
+          <NavLink to="/modalidades">Modalidades</NavLink>
         </li>
         <li>
-            <NavLink to="/planos">Planos <span className="year-tag">2024</span></NavLink>
+          <NavLink to="/planos">
+            Planos <span className="year-tag">2024</span>
+          </NavLink>
         </li>
         <li>
-            <NavLink to="/galeria">Galeria</NavLink>
+          <NavLink to="/galeria">Galeria</NavLink>
         </li>
         <li>
-            <NavLink to="/contato">Contato</NavLink>
+          <NavLink to="/contato">Contato</NavLink>
         </li>
       </ul>
     </nav>
